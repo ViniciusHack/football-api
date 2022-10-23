a09d90ff82a5fc8b4bb5dac4889ab02eb392754e
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-10-23 18:41:29 -0300

    feature: insert new matches

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
-import { PrismaClient } from '@prisma/client';
+import { Matches, PrismaClient } from '@prisma/client';
+import { compareAsc } from 'date-fns';
 import express from 'express';
 
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/finished", async (req, res) => {
-  const result = await prisma.matches.findMany({
-    where: {
-      finished: true
-    }
+app.get("/matches/finished", async (req, res) => {
+  const matches = await prisma.matches.findMany() // Processando todos as partidas, não tão performático?
+
+  const finishedMatches = matches.filter(match => compareAsc(match.date, new Date()))
+
+  return res.json({ matches: finishedMatches })
+})
+
+interface Match {
+  id: number;
+}
+
+app.post("/matches", async (req, res) => {
+  const matches = req.body.matches as Matches;
+
+  await prisma.matches.createMany({
+    data: matches
   })
 })
